7a7274c0da9c5790cb373ce41e5ca5ab59e5b9d3
paulolisboa38 <paulolisboa38@gmail.com>
2023-09-16 02:49:32 -0300

    updating app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'exercicios';
+  title = 'Lista Exercícios Angular 01';
+
+  mostrarOcultar: string = "Mostrar/Ocultar Texto";
+  mostrarTexto: boolean = false;
+  texto: string = "Este é o texto que será mostrado ou ocultado.";
+
+  alternarTexto() {
+    this.mostrarTexto = !this.mostrarTexto;
+    this.mostrarOcultar = this.mostrarTexto ? "Ocultar Texto" : "Mostrar Texto";
+  }
 }
