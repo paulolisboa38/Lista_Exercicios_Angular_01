33c0fc83298a3387293a243660ee4d43c5696b98
paulolisboa38 <paulolisboa38@gmail.com>
2023-09-17 07:04:30 -0300

    updating app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ListaTarefasComponent } from './lista-tarefas/lista-tarefas.component';
 import { Exercicio03Component } from './exercicio03/exercicio03.component';
+import { Exercicio04Component } from './exercicio04/exercicio04.component';
+import { Exercicio05Component } from './exercicio05/exercicio05.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaTarefasComponent,
-    Exercicio03Component
+    Exercicio03Component,
+    Exercicio04Component,
+    Exercicio05Component
   ],
   imports: [
     BrowserModule,
