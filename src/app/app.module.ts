296e1bff2db1c4edfb63a7c7e6c92633cb17b7c8
paulolisboa38 <paulolisboa38@gmail.com>
2023-09-18 01:22:30 -0300

    updating app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { FiltrarPipe } from './pipes/filter.pipe';
 import { ListaTarefasComponent } from './lista-tarefas/lista-tarefas.component';
 import { Exercicio03Component } from './exercicio03/exercicio03.component';
 import { Exercicio04Component } from './exercicio04/exercicio04.component';
@@ -10,14 +11,15 @@ import { Exercicio05Component } from './exercicio05/exercicio05.component';
 import { Exercicio06Component } from './exercicio06/exercicio06.component';
 import { Exercicio07Component } from './exercicio07/exercicio07.component';
 import { Exercicio08Component } from './exercicio08/exercicio08.component';
-import { FiltrarPipe } from './pipes/filter.pipe';
 import { Exercicio09Component } from './exercicio09/exercicio09.component';
 import { Exercicio10Component } from './exercicio10/exercicio10.component';
 import { Exercicio11Component } from './exercicio11/exercicio11.component';
+import { Exercicio12Component } from './exercicio12/exercicio12.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FiltrarPipe,
     ListaTarefasComponent,
     Exercicio03Component,
     Exercicio04Component,
@@ -25,10 +27,10 @@ import { Exercicio11Component } from './exercicio11/exercicio11.component';
     Exercicio06Component,
     Exercicio07Component,
     Exercicio08Component,
-    FiltrarPipe,
     Exercicio09Component,
     Exercicio10Component,
-    Exercicio11Component
+    Exercicio11Component,
+    Exercicio12Component
   ],
   imports: [
     BrowserModule,
